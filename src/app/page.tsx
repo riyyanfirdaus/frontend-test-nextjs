3f931eaa8ed9d028e8adcba87d34210a8c227a10
riyyanfir <m.riyyan.firdaus@gmail.com>
2024-06-24 22:35:38 +0700

    feat: add hero section, content section, and promotion section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,83 @@
 import Image from "next/image";
+import HeroImg from "@/assets/hero-img.webp";
+import BukalapakLogo from "@/assets/bukalapak-logo.png";
+import GojekLogo from "@/assets/gojek-logo.png";
+import PersonOne from "@/assets/person-1.webp";
+import PersonTwo from "@/assets/person-2.webp";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-w-[1200px] w-full mx-auto">
+      <div className="min-h-screen px-4 py-6 md:flex md:items-center md:px-0">
+        <div className="space-y-6">
+          <p className="text-xl">Hi, Riyyan Selamat datang di</p>
+          <p className="font-bold text-3xl">Digital Marketing Job Guarantee Bootcamp</p>
+          <p className="font-base">"Raih karier impian dengan pelatihan intensif dan ekslusif bersama para high-achievers dengan jaminan 100% uang kembali"</p>
+          <p className="font-bold text-base">100% Alumni Job Guarantee Program telah bekerja di:</p>
+          <div className="overflow-x-hidden">
+            <div className="flex animate-marquee">
+              <Image src={BukalapakLogo} className="scale-50" alt="bukalapak logo" />
+              <Image src={GojekLogo} className="scale-50" alt="gojek logo" />
+            </div>
+          </div>
         </div>
+        <Image src={HeroImg} className="object-contain h-fit" alt="hero image" />
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="min-h-72 mb-32 py-10 md:flex md:gap-x-6 md:mb-0">
+        <div className="w-full flex flex-col items-center bg-gradient-to-b from-teal-600 to-teal-900 min-h-72 rounded-t-lg px-4 py-6">
+          <h2 className="font-semibold text-white text-xl">Pengalaman Alumni</h2>
+          <div className="flex flex-wrap justify-center gap-y-6 mt-10 md:flex-nowrap md:gap-x-6">
+            <div className="bg-white w-full px-4 py-6 rounded-md space-y-4 md:w-3/5">
+              <div className="flex items-center gap-x-3">
+                <Image src={PersonOne} className="w-12 h-12 object-cover rounded-full" alt="person one" />
+                <div>
+                  <p className="font-semibold">Stephanie Liveran</p>
+                  <p className="text-sm text-gray-400">Company One</p>
+                </div>
+              </div>
+              <p>
+                While attending the Digital Marketing Bootcamp at Unknown Academy, I gained a lot of experience and knowledge related to digital marketing. For me, who does not have an educational background related to marketing, I feel
+                challenged and interested in continuing to expand my knowledge about marketing and business. I’m grateful to Unknown Academy which has become a forum for job seekers, or fresh graduates like me, to be able to learn skills
+                and practices that cannot be obtained in lectures.
+              </p>
+            </div>
+            <div className="bg-white w-full px-4 py-6 rounded-md space-y-4 md:w-3/5">
+              <div className="flex items-center gap-x-3">
+                <Image src={PersonTwo} className="w-12 h-12 object-cover rounded-full" alt="person one" />
+                <div>
+                  <p className="font-semibold">Craig Mckay</p>
+                  <p className="text-sm text-gray-400">Company Two</p>
+                </div>
+              </div>
+              <p>
+                While attending the Digital Marketing Bootcamp at Unknown Academy, I gained a lot of experience and knowledge related to digital marketing. For me, who does not have an educational background related to marketing, I feel
+                challenged and interested in continuing to expand my knowledge about marketing and business. I’m grateful to Unknown Academy which has become a forum for job seekers, or fresh graduates like me, to be able to learn skills
+                and practices that cannot be obtained in lectures.
+              </p>
+            </div>
+          </div>
+          <button className="bg-teal-400 text-white px-6 py-2 mt-6 rounded-md hover:bg-teal-500">Lihat lebih banyak testimoni</button>
+        </div>
+        <aside className="w-full h-fit bg-white fixed bottom-0 flex items-end justify-between px-4 py-6 z-30 shadow-lg md:w-72 md:sticky md:top-0 md:flex-col md:items-start md:justify-start md:gap-y-2">
+          <div className="h-full hidden md:block">
+            <p className="text-gray-400">Detail</p>
+            <ul className="mt-3 space-y-2 pb-20 border-b border-gray-500">
+              <li className="hover:text-gray-700">Pengalaman Alumni</li>
+              <li className="hover:text-gray-700">FAQ</li>
+            </ul>
+          </div>
+          <div className="space-y-2">
+            <p>Online Bootcamp</p>
+            <p className="text-gray-400 line-through">Rp 4.499.999</p>
+            <div className="flex items-center gap-x-1">
+              <span className="bg-red-500 text-white text-sm rounded-lg p-2">22%</span>
+              <p className="text-yellow-500">Rp 3.499.999</p>
+            </div>
+          </div>
+          <div className="mt-3">
+            <button className="bg-yellow-400 rounded-lg shadow-md px-3 py-1">Daftar Sekarang</button>
+          </div>
+        </aside>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      fontFamily: {
+        nunito: ["var(--font-nunito)"],
       },
     },
   },
